6e091cd203987872ab20cf5349e578abc6366577
semsepi <77001486+sepsemi@users.noreply.github.com>
2023-04-10 17:46:19 +0000

    Update main.py
    Now properly handles:
    1) Disconnect
    2) Connect
    3) Reconnect
    3) Requesting more data
    6) Carrier loss
    6) Errors
    
    This is a work in progress

--- main.py
+++ main.py
@@ -1,188 +1,213 @@
-import re 
 import os
-import sys
+import re
 import time
-import serial
 import logging
+import contextlib
+from serial import Serial, SerialException
+
+LTE_INTEFACE = 'eth1'
+SERIAL_PORT = '/dev/ttyACM0'
+DATAPLAN_APN = 'internet.kpn.de'
 
 IPV4_PATTERN = re.compile(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})')
-LTE_INTEFACE = 'eth1'
-WAN_DEFAULT_GATEWAY = '192.168.0.1'
 
-SERIAL_PATH = sys.argv[1]
-
-# Setup basic logging config
 logging.basicConfig(
-        format='[%(asctime)-8s][%(funcName)-8s()] %(levelname)s: %(message)s',
-    level=logging.INFO,
-    datefmt='%Y-%m-%d %H:%M:%S')
+    format='[%(asctime)-8s][%(funcName)-8s()] %(levelname)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    level=logging.DEBUG
+)
 
 def iproute2_set_network_address(address):
     # iptoute2 ip addr flush deletes default routes too
     system_commands = [
         'ip link set {} up'.format(LTE_INTEFACE),
-        'ip route del default', 
+        'ip route del default',
         'ip addr flush {}'.format(LTE_INTEFACE),
         'ip addr add {}/24 broadcast + dev {}'.format(address, LTE_INTEFACE),
         'ip route add default via {}'.format(address),
-        'ip link set arp off dev {}'.format(LTE_INTEFACE)
+        'ip link set arp off dev {}'.format(LTE_INTEFACE),
     ]
 
     for command in system_commands:
         os.system(command)
 
-class ModemConnection:
-    def __init__(self, transport):
-        self.transport = transport
-        self.commands = [
-            'AT+CFUN=1',
-            'AT+CMEE=2',
-            'AT+XLEC=1',
-            'AT+CGPIAF=1,0,0,0',
-            'AT+XDNS=1,1',
-            'AT+CGDCONT=1,"IP","internet.kpn.de"',
-            'AT+CGACT=1,1',
-            'AT+CGPADDR=1',
-            'AT+XDATACHANNEL=1,1,"/USBCDC/0","/USBHS/NCM/0",2,1',
-            'AT+CGDATA="M-RAW_IP",1',
-            'AT+CNMI=2,2',
-            'AT+CMGF=1'
-        ]
+def get_interface_status(interface):
+    with open('/sys/class/net/{}/operstate'.format(interface)) as fp:
+        return fp.read(32).rstrip()
 
-    @property
-    def sim_mode(self):
-        # returns cfun mode
-        raise NotImplementedError
+class ModemConnection:
+    COMMANDS = [
+        'AT+CFUN=1',
+        'AT+CMEE=2',
+        'AT+CGPIAF=1,0,0,0',
+        'AT+XDNS=1,1',
+        'AT+CGDCONT=1,"IP",{}'.format(DATAPLAN_APN),
+        'AT+COPS=0',
+        'AT+CGACT=1,1',
+        'AT+CGPADDR=1',
+        'AT+XDATACHANNEL=1,1,"/USBCDC/0","/USBHS/NCM/0",2,1',
+        'AT+CGDATA="M-RAW_IP",1',
+        'AT+CNMI=2,2',
+        'AT+XLEC=1',
+        'AT+CMGF=1',
+    ]
+
+    BUFFER_SIZE = 1024
+    # Timeout for initial connection, modem can hang
+    INITIAL_READ_TIMEOUT = 2.0
+
+    # received messages
+    CELL_INFO = 'XLECI:'
+    NO_CARRIER = 'NO CARRIER'
+    SET_IP_ADDRESS = '+CGPADDR:'
+    REQUEST_MORE_DATA = 'activeren?'
+
+    def __init__(self, serial):
+        self.connected = False
+        self.serial = serial
+        self.last_ack = time.perf_counter()
 
     def initialize(self):
-        while not self.transport.operating:
-            for command in self.commands:
-                self.transport.send(command)
-                self.recvieved_message(self.transport.receive(4))
- 
-    def sms_get_more_data(self):       
-        # We can send 10 messages to get a total of 20 gigs without opening their application on a phone
-        logging.debug('sending sms to carrier to get more data')
-        
-        for command in ['AT+CMGS="1280",129', 'NOG 1GB','\x1A']:
-            self.transport.send(command)
+        # initialize the modem for operation
+        for command in self.COMMANDS:
+            self.send_and_acknowledge(command)
 
-    def adb_interact_more_data(self):
-        # What this should do is open an application
-        # Click something in the application (random location) 
-        # Then click on the button it is supposed to
-        # The its going to click on a button again after waiting x seconds
-        # Then its going to swipe from x to y in pos x and z in 150 miliseconds
-        # Then its going to click a button
-        # Then it will click once more
-        
-        # This will happen via an remote adb connection
-        raise NotImplementedError
-                
-    def recvieved_message(self, msg):
-        if not msg:
-            return None
-        
-        if not self.transport.operating:
-            if '+CGPADDR: 1,' in msg:
-                ipv4_search = IPV4_PATTERN.search(msg)
-
-                if ipv4_search is not None:
-                    ipv4_address = ipv4_search.group(0)
-                    if ipv4_address == '0.0.0.0':
-                        return None
-
-                    iproute2_set_network_address(ipv4_address)
-                    logging.info('modem nat ipv4 address: ({})'.format(ipv4_address))
-        
-                    self.transport.operating = True
-                    logging.info('Modem is operational')
-
-            # We are still not in a operating state no need to listen for events
+    def set_ipv4(self, message):
+        ipv4_search = IPV4_PATTERN.search(message)
+        if ipv4_search is None:
             return None
 
-        if 'NO CARRIER' in msg:
-            # We should switch to flightmode and disconnect to then reinitiate connection
-            logging.warning('carrier lost entering flight mode')
-            self.transport.send('AT+CFUN=4')
-            self.transport.operating = False
-            self.initialize()
-
-        if 'activeren?' in msg:
-            # We got an message saying we went though our data
-            # We can send 10 messages to get a total of 20 gigs without opening their application on a phone
-            logging.info('no more data requesting more') 
-            self.sms_get_more_data()
+        ipv4_address = ipv4_search.group(0)
+        if ipv4_address == '0.0.0.0':
+            return None
         
-        # Carrier agregation/cell info
-        if '+XLECI:' in msg:
-            logging.debug(msg[msg.find('+XLECI:'):])
+        logging.info('NAT address: ({})'.format(ipv4_address))
+        iproute2_set_network_address(ipv4_address)
 
-        logging.debug(msg)
+    def send_and_acknowledge(self, string, timeout=2):
+        # try and send a message to the serial port
+        self.serial.write('{}\r\n'.format(string).encode()) 
 
-class SerialModem:
-    def __init__(self, path, baudrate, timeout=0.1):
-        self.path = path
-        self.baudrate = baudrate
-        self.timeout = timeout
-        self.operating = False
-        self.transport = self.create_connection()
-
-    def create_connection(self):
-        device = serial.Serial(
-            port = self.path,
-            baudrate = self.baudrate,
-            timeout = self.timeout
-        )
-        return device
-
-    def close_connection(self):
-        self.closed = True
-        self.transport.close()
- 
-    def send(self, data):
-        self.transport.flush()
-        formated = '{}{}'.format(data, '\r\n')
-        self.transport.write(formated.encode())
-
-    def receive(self, timeout=2):
-        buffer = ''
-        lines_buffered = 0
-        stime = time.perf_counter()
-        
-        # Reset serial console buffer
-        self.transport.reset_output_buffer()
-        self.transport.reset_input_buffer()
-
+        # block until we get OK response
         while True:
-            try:
-                data = self.transport.readline().decode()
-            except serial.serialutil.SerialException:
-                self.close_connection()
+            message = self.read()
+            if 'OK' in message or len(message) < 8:
                 break
 
-            if time.perf_counter() - stime>= timeout:
-                return buffer 
+            self.recvieved_message(message)
 
-            if data:
-                lines_buffered+=1
-                buffer+=data
+    def is_interface_up(self):
+        status = get_interface_status(LTE_INTEFACE)
+        return True if status == 'up' else False 
+ 
+    def send(self, string):
+        self.serial.write('{}\r\n'.format(string).encode()) 
 
-            if lines_buffered >= 2:
-                return buffer
+    def read(self):
+        try:
+            data = self.serial.readline(self.BUFFER_SIZE)
+            return data.decode().rstrip()
+        except UnicodeDecodeError:
+            # This is unhandled
+            return ''
 
-def main():
-    modem = SerialModem(SERIAL_PATH, 115200, timeout=1)
-    while True:
+    def clear(self):
+        # clear the buffers of the serial device
+        self.serial.reset_output_buffer()
+        self.serial.reset_input_buffer()
+
+    def request_more_data(self):
+        for string in ['AT+CMGS="1280",129', 'NOG 1GB','\x1A']:
+            self.send(string)
+
+    def recvieved_message(self, message):
+        if len(message) > 1:
+            self.last_ack = time.perf_counter()
+            logging.debug('message: {}'.format(message))
+
+        if self.SET_IP_ADDRESS in message:
+            self.set_ipv4(message)
         
-        connection = ModemConnection(modem)
-        if not modem.operating:
-            # The modem is not running or has been reset
-            connection.initialize()
+            if self.is_interface_up():
+                self.connected = True
 
-        else:
-            # Start polling for more
-            data = modem.receive()
-            connection.recvieved_message(data)
+        elif self.NO_CARRIER in message:
+            # The connection with the tower has been lost, we enter flightmode to reset the connection
 
-main()
+            self.connected = False
+            self.send_and_acknowledge('AT+CFUN=4')
+            logging.warning('Carrier lost, entering flight mode')
+
+        elif self.REQUEST_MORE_DATA in message:
+            logging.info('Dataplan: Requesting more data')
+            self.request_more_data()
+
+        elif self.CELL_INFO in message:
+            # Carrier agregation/cell info
+            logging.debug('carrier cell: {}'.format(message[message.find('+XLECI:'):]))
+
+    def connect_and_poll(self):
+        # Bruteforce the initialization
+        while not self.connected:
+            # we can't wait forever, reset connection
+            if self.last_ack + self.INITIAL_READ_TIMEOUT < time.perf_counter():
+                return None
+
+            self.initialize()
+        
+        # we are operational and connected
+        logging.info('Connected')
+
+        while self.connected is True:
+            data = self.read()
+            if data is None:
+                break
+            
+            self.recvieved_message(data)
+
+class Client:
+    def __init__(self, port, timeout=0.1, rate=115200):
+        self.port = port
+        self.timeout = timeout
+        self.rate = rate
+
+    def create_serial(self):
+        return Serial(
+            dsrdtr=True,
+            rtscts=True,
+            port=self.port, 
+            timeout=self.timeout, 
+            baudrate=self.rate
+        )
+
+    def connect(self, reconnect):
+        while reconnect is True:
+            if not os.path.exists(self.port):
+                time.sleep(self.timeout)
+                continue
+
+            # wait until the port is found
+            self._serial = self.create_serial()
+            self._connection = ModemConnection(self._serial)
+
+            # reset the buffer
+            self._connection.clear()
+
+            # start polling for data
+            try:
+                self._connection.connect_and_poll()
+            except SerialException:
+                logging.warning('Forcefull detatch')
+                self._serial.close()
+                time.sleep(self.timeout)
+                continue
+
+            # the device not has been lost
+            loging.warning('Connection lost: reconnecting')
+            self._serial.close()
+
+    def run(self, reconnect=True):
+        self.connect(reconnect)
+
+client = Client('/dev/ttyACM0')
+client.run()
